2b3a5206cdd7fca5b380e2be7fd4f1b6ba5cbb01
Gabriel Bastos <gabrielbastosdev@gmail.com>
2025-04-10 15:11:47 -0300

    Added changes to routes.py

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Form, HTTPException, File, UploadFile, Cookie,status
+import json
+from fastapi import FastAPI, Form, HTTPException, File, UploadFile, Cookie, status
 from fastapi.responses import JSONResponse
 from typing import Annotated, Dict
 import pickle, datetime
@@ -7,19 +8,20 @@ router = FastAPI()
 
 
 @router.post("/resume_file/")
-async def create_upload_file(session_id: str = Cookie(), opportunity: str = Form(...), cv: UploadFile = File(None)) -> Dict[str,str]:
-    if session_id is None or not router.state.redis.get(session_id):
+async def create_upload_file(session_id: str = Cookie(), opportunity: str = Form(...), cv: UploadFile = File(...)) -> Dict[str,str]:
+    if session_id is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Session ID cookie is required",
         )
+    router.state.redis.get(session_id)
     try:
         contents = await cv.read()
         file_obj = {
-            "cv": pickle.dumps(contents),  # The raw bytes of the PDF
+            "cv": contents,  # The raw bytes of the PDF
             "opportunity": opportunity 
         }
-        router.state.redis.set(session_id, file_obj, ex=datetime.timedelta(minutes=30)) 
+        router.state.redis.set(session_id, pickle.dumps(file_obj), ex=datetime.timedelta(minutes=30)) 
         return JSONResponse(status_code=200, content="CV and Opportunity uploaded!")
         
     except Exception as e:

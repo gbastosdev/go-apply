001fa47444ceb78825038d6c2b4572b013eea5de
Gabriel Bastos de Freitas <gabrielbastosdev@gmail.com>
2025-09-03 19:43:11 -0300

    Autorizando o fabo.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI , Request, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import routes
 
+origins = [
+    "http://localhost",
+    "https://go-apply-production.up.railway.app"
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount('/api', routes.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
